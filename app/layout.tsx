520fe5f1e100d34b31aab93b0e77809441499103
salimsayed <salemsayed981@gmail.com>
2023-12-15 16:01:50 +0200

    UPD framer motion

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+
+import "./globals.css";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Anime Vault",
+  description: "Your favorite anime, all in one place.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={dmSans.className}suppressHydrationWarning={true}> 
+        <main className="max-w-7xl mx-auto bg-[#81848d]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ components/MotionDiv.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { motion } from "framer-motion";
+
+const MotionDev = motion.div;
+
+export default MotionDev;
