6eea95b890c66aacceb629f709bbb824021e41f7
salimsayed <salemsayed981@gmail.com>
2023-12-15 17:35:26 +0200

    UPD Load More

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,35 +1,57 @@
 "use client";
-import { fetchAnime } from "@/app/action";
+
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
- 
- 
-export type  AnimeCard=JSX.Element
+import { useEffect, useState } from "react";
+
+import { fetchAnime } from "../app/action";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
+
 function LoadMore() {
   const { ref, inView } = useInView();
-  const [page,setPage]=useState(2);
+
   const [data, setData] = useState<AnimeCard[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     if (inView) {
-      fetchAnime(page).then((res) => setData([...data, ...res ]));
-      setPage(page+1);
+      setIsLoading(true);
+      // Add a delay of 500 milliseconds
+      const delay = 500;
+
+      const timeoutId = setTimeout(() => {
+        fetchAnime(page).then((res) => {
+          setData([...data, ...res]);
+          page++;
+        });
+
+        setIsLoading(false);
+      }, delay);
+
+      // Clear the timeout if the component is unmounted or inView becomes false
+      return () => clearTimeout(timeoutId);
     }
-  }, [inView, data,page]);
+  }, [inView, data, isLoading]);
+
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-      {data}
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
+          {inView && isLoading && (
+            <Image
+              src="./spinner.svg"
+              alt="spinner"
+              width={56}
+              height={56}
+              className="object-contain"
+            />
+          )}
         </div>
       </section>
     </>
